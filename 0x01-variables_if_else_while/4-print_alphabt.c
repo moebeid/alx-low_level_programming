a4f190239e8e3037bde2a2f2bc473bdbdf285175
Mohamed Mohey <mebeid554@gmail.com>
2022-12-09 17:16:53 -0800

    except2

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -11,13 +11,13 @@
 int main(void)
 {
 		char letter;
-
 		for (letter = 'a'; letter <= 'z'; letter++)
 		{
 		if (letter != 'e' && letter != 'q')
-			putchar(letter);
+		{
+		putchar(letter);
+		}
 		}
 		putchar('\n');
-
 	return (0);
 }
